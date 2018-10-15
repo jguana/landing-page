8978e02ef0586617a94c22e5efd7ddac0e260755
gimjon <kim.jy.john@gmail.com>
2018-10-15 15:28:35 -0700

    Remove merge

--- server.js
+++ server.js
@@ -26,15 +26,11 @@ app.use("/api/items", items);
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-<<<<<<< HEAD
-  app.use(express.static('client/build'));
-=======
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
->>>>>>> c1aa268274fb46970291efaca2e583f1b3083cd4
 }
 
 const port = process.env.PORT || 5000;
