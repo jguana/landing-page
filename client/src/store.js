8416b97f7e749c0d8f04d818e6ae9c6465073671
gimjon <kim.jy.john@gmail.com>
2018-10-20 00:39:05 -0700

    Add __

--- client/src/store.js
+++ client/src/store.js
@@ -6,7 +6,7 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const composeEnhancers = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
   initialState,
