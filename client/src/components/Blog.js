01348b5a043d8fcf0a6ac49616ee0db7dd4b5ee6
gimjon <kim.jy.john@gmail.com>
2018-10-23 22:58:56 -0700

    Implement vw styles to blog

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -2,10 +2,16 @@ import React, { Component } from "react";
 
 class Blog extends Component {
   render() {
+    const h2 = {
+      fontSize: "8vw"
+    };
+    const p = {
+      fontSize: "3vw"
+    };
     return (
       <div>
-        <h2>HELLO</h2>
-        <p>
+        <h2 style={h2}>HELLO</h2>
+        <p style={p}>
           Cras facilisis urna ornare ex volutpat, et convallis erat elementum.
           Ut aliquam, ipsum vitae gravida suscipit, metus dui bibendum est, eget
           rhoncus nibh metus nec massa. Maecenas hendrerit laoreet augue nec
