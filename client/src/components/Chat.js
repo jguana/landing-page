ba07ba92ca085419c49f91b8f99b3bb8988dd65f
John Kim <kim.jy.john@gmail.com>
2018-11-01 16:11:13 -0700

    Implement media query

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import Media from "react-media";
 
 class Chat extends Component {
   render() {
     return (
       <div>
-        <h2>todo: chat component</h2>
+        <Media query="(max-width: 599px)">
+          {matches =>
+            matches ? (
+              <p>The document is less than 600px wide.</p>
+            ) : (
+              <p>The document is at least 600px wide.</p>
+            )
+          }
+        </Media>
       </div>
     );
   }
